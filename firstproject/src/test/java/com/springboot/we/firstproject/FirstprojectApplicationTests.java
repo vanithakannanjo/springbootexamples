43134afb0238e7169a1a0e4b8d7a8fcf0ac1b87f
Vanitha Kannan <vkgenps@gmail.com>
2024-09-21 17:16:34 +0530

    test

---
+++ firstproject/src/test/java/com/springboot/we/firstproject/FirstprojectApplicationTests.java
@@ -0,0 +1,14 @@
+package com.springboot.we.firstproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstprojectApplicationTests  {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
